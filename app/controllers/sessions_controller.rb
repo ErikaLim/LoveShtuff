db72fbe4b69aaf97f5dff5802542aa77c1f03c36
ErikaLim <Erika.megan.lim@gmail.com>
2015-02-06 08:45:47 -0800

    fixed views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,8 @@
 class SessionsController < ApplicationController
+  def new
+
+  end
+
   def create
     auth_hash = request.env['omniauth.auth']
 
@@ -10,12 +14,21 @@ class SessionsController < ApplicationController
       user.authorizations.build :provider => auth_hash["provider"], :uid => auth_hash["uid"]
       user.save
 
-      render :text => "Hi #{user.first_name}! You've signed up."
+      redirect_to root_path
     end
   end
 
   def failure
 
+  end
+
+  def index
+    @foursquare = []
+    response = RestClient.get('https://api.foursquare.com/v2/users/self/checkins?oauth_token=2TIM1DJ1POSQC2T3SF01YA2XX4GBPDFWT5YRTFZA40PYBNJ4&v=20150205')
+    @checkins_hash = JSON.parse(response.body)["response"]["checkins"]["items"]
+    @checkins_hash.each do |c|
+      puts c["venue"]["name"]
+    end
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,25 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @foursquare = []
+    response = RestClient.get('https://api.foursquare.com/v2/users/self/checkins?oauth_token=2TIM1DJ1POSQC2T3SF01YA2XX4GBPDFWT5YRTFZA40PYBNJ4&v=20150205')
+    @checkins_hash = JSON.parse(response.body)["response"]["checkins"]["items"]
+    @checkins_hash.each do |c|
+      puts c["venue"]["name"]
+    end
+      # puts url
+      # begin
+      #   data = RestClient.get(url)
+      #   res = JSON.parse(data.body)
+      #   res.each do |foursquare|
+      #     @foursquare << foursquare["items"]
+
+        # end
+      # rescue => e
+      #   puts e.response
+      # end
+
+    # end
   end
 
   def new
